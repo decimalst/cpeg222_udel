1864fbb3400f820b827f50e6e06fbf48be9792a9
decimalst <byronl@udel.edu>
2015-04-20 13:30:35 -0400

    Added comments and TODOs
    Added comments, and some of the state logic, as well as some of the ADC
    configuration functions from the lecture

--- Project3/Code/Project3-template.c
+++ Project3/Code/Project3-template.c
@@ -1,3 +1,15 @@
+// CPEG222 - Project 2 Code
+// Author: Byron Lambrou, Andrew Lipman
+// Input: Keypad PMod, Microphone Pmod
+// Output:  2xSSD pmods
+// Comments:
+
+/* TODO:1.configure pin mapping*/
+/* TODO:2.set up the timer interrupts*/
+/* TODO:3.set up the adc*/
+/* TODO:4.program in the state logic*/
+/* TODO:5.set up change notice interrupts for keypad*/
+
 #include <p32xxxx.h>
 #include <plib.h>
 #include "dsplib_dsp.h"
@@ -35,7 +47,7 @@ int16c sampleBuffer[N];
 int16c scratch[N];
 
 /* intermediate array holds computed FFT until transmission*/
-int16c dout[N]; 
+int16c dout[N];
 
 /* intermediate array holds computed single side FFT */
 int singleSidedFFT[N];
@@ -49,8 +61,68 @@ int freq=0;
 // Function definitions
 int computeFFT(int16c *sampleBuffer);
 
+//Timer 5 interrupt, we can use this for the first interrupt
+void __ISR(_TIMER_5_VECTOR, ipl4) _T5Interrupt(void)
+{
+
+	IFS0CLR = 0x100000;   // Clear Timer5 interrupt status flag (bit 20)
+}
+
+//Timer 4 interrupt, we can use this for the second interrupt
+//Note that we'll want to change either the Timer 5 ipl or Timer4 ipl
+void __ISR(_TIMER_4_VECTOR, ipl4) _T4Interrupt(void)
+{
+
+	IFS0bits.T4IF = 0;   // Clear Timer4 interrupt status flag
+}
+
+void initADC (int amask)
+{
+  //ADC automatic confug
+  AD1PCFG = 0xFF7F; //All PORTB = digital but RB7 = analog
+  AD1CON1 = 0x00E0; //Automatic conversion after sampling
+  AD1CHS = 0x00070000; //Connect RB7/AN7 as CH0 input
+  AD1CSSL = 0; //No scanning required
+  AD1CON2 = 0; //Use MUXA, AVss/AVdd used as Vref+/-
+  AD1CON3 = 0x1F3F; //Tad=128 x Tpb, Sample time=31 Tad
+  AD1CON1bit.ADON = 1; //turn on the ADC
+}
+int readADC ( int ch )
+{
+  AD1CON1bits.SAMP=1; //1. start sampling
+  while (!ad1CON1bits.DONE); //2. Wait until done
+  return ADC1BUF0; //3. read conversion result
+}
+
+int mode=1;
+int passcode=0;
+
+
 main(){
         int i;
+
+        /* These values should be changed to reflect our timer config!*/
+        // Configure Timer 5.
+        T5CONbits.ON = 0; // Stop timer, clear control registers
+        TMR5 = 0; // Timer counter
+        PR5 = 0xC000; //Timer count amount for interupt to occur
+        IPC5bits.T5IP=0; //prioirty 4
+        IFS0bits.T5IF=0; // clear interrupt flag
+        T5CONbits.TCKPS=0;  // prescaler at 1:256, internal clock sourc
+        T5CONbits.ON=1;  // Timer 5 module is enabled
+        IEC0bits.T5IE=1; //enable Timer 5
+
+
+        //Configure Timer 4
+        T4CONbits.ON=0; //Turn Timer 4 off
+        TMR4=0; //Clear Timer 4 register
+        T4CONbits.TCKPS=3; //Select prescaler = 256
+        T4CONbits.TCS=0; //Select internal clock
+        PR4=901250; //Load period Register
+        IFS0bits.T4IF=0; //Clear Timer 4 interupt flag
+        IPC4bits.T4IP=4; //Set priority level to 4
+        IEC0bits.T4IE=1; // Enable Timer 4
+        T4CONbits.ON=1; //Turn Timer  4 on.
         /* assign values to sampleBuffer[] */
         for (i=0; i<N; i++)
         {
@@ -62,10 +134,47 @@ main(){
 	{
             freqVector[i] = i*(SAMPLE_FREQ/2)/((N/2) - 1);
 	}
-        
+
 while(1){
         /* get the dominant frequency */
         freq=freqVector[computeFFT(sampleBuffer)];
+
+        /* Current state logic here*/
+				switch(mode):
+					case 1:
+					/* Pmod msd should show 's', and last 3 digits should show passcode */
+
+						break;
+					case 2:
+					/* Pmod msd should show 'u', and last 3 digits should show off */
+						break;
+					case 3:
+					/* Pmod msd should show 'L', and last 3 digits should show entered pass */
+						break;
+					case 4:
+					/* Pmod msd should Flash "AAAA" */
+						break;
+
+
+        /* Next state logic here*/
+				switch(mode):
+					case 1:
+					/* 0-9 should input digit, 'C' should clear, 'D' should delete, */
+					/* 'E' enter if valid input pass*/
+						break;
+					case 2:
+					/* if a button is pressed and held for >=1 second, go back to mode 1 */
+					/* if a button is pressed and held for <1 second, go to mode 3 */
+						break;
+					case 3:
+					/* 0-9 should input digit, 'C' should clear, 'D' should delete, */
+					/* 'E' enter if valid input pass. If pass is correct, enter mode 2*/
+					/* if pass incorrect, enter mode 4. If microphone frequency is equal */
+					/* to pass frequency, enter mode 2, else to mode 4. */
+						break;
+					case 4:
+					/* If SSDs have been flashing for 5 seconds, then enter mode 3.*/
+						break;
 }
 }
 
@@ -74,7 +183,7 @@ int computeFFT(int16c *sampleBuffer)
 {
 	int i;
         int dominant_freq=1;
-        
+
 	/* computer N point FFT, taking sampleBuffer[] as time domain inputs
          * and storing generated frequency domain outputs in dout[] */
 	mips_fft16(dout, sampleBuffer, fftc, scratch, log2N);
